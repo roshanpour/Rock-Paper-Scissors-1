c0431bcc5804724460cbf835d8be072cb6963557
Roshan <roshan.pourghasemi13@gmail.com>
2022-09-04 16:52:08 -0400

    Added UI

--- script.js
+++ script.js
@@ -12,24 +12,25 @@ function getComputerChoice() {
 }
 
 /* runs a round of Rock, Paper, Scissors */
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
+    let playerSelection = e.target.textContent;
+    let computerSelection = getComputerChoice();
     if (playerSelection == "Scissors" && computerSelection == "Paper"){
-        return "You win! Scissors beats Paper!";
+        result.textContent = "You win! Scissors beats Paper!";
     } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        return "You lose! Scissors beats paper";
+        result.textContent = "You lose! Scissors beats paper";
     } else if (playerSelection > computerSelection){
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if (playerSelection < computerSelection) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return "It's a tie!";
+        result.textContent = "It's a tie!";
     }
-
-
+    update();
 }
 
 /* runs five rounds of the game */
-function game() {
+/* function game() {
     let playCount = 0;
     let computerCount = 0;
     for (let i = 0; i < 5; i++){
@@ -60,4 +61,36 @@ function game() {
 }
 
 /* calls function to play game */
-game();
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const player = document.querySelector('.display1');
+const computer = document.querySelector('.display2');
+
+let playerCounter = 0;
+let compCouter = 0;
+
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+}); 
+function update(){
+    if (result.textContent.slice(4,7) == "win"){
+        playerCounter++;
+    } else if (result.textContent.slice(4, 8) == "lose"){
+        compCouter++;
+    }
+    if (playerCounter == 5){
+        result.textContent == "You win the game!"
+        playerCounter = 0;
+        compCouter = 0;
+    } else if (compCouter == 5){
+        result.textContent == "You lose the game!"
+        playerCounter = 0;
+        compCouter = 0;
+    }
+    player.textContent = playerCounter;
+    computer.textContent = compCouter;
+}
+
+
